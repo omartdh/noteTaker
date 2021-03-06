e11b4d761e55461914a451435963dcca4f9cacd0
Omar Dahmash <omartdh@Omars-MacBook-Pro.local>
2021-03-05 22:26:19 -0800

    update dynamic port

--- Develop/server.js
+++ Develop/server.js
@@ -4,7 +4,7 @@ const notes = require('./db/db.json');
 const fs = require('fs');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
